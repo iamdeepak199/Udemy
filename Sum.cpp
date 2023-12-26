dc68c1827f5d1de5e138e9c8e6e27bff41504a99
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2023-12-26 15:36:45 +0530

    Sum of N numbers
    Enter the number to find it's Sum : 10
    
    Sum of N numbers will be 55

---
+++ Sum.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void fastio(bool read = false) {
+	if(read) {
+		#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+		freopen("error.txt","w",stderr);
+		#endif
+	}
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	return;
+}
+
+int main(){
+    fastio(true);
+    int N,sum=0;
+    cin >> N;
+    for(int i=1;i<=N;i++){
+        sum=sum+i;
+    }
+    cout<<"Sum of N numbers will be "<<sum;
+
+}
