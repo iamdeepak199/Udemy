323bbcb8102e80b539dcc83efef9e40b79c8dd83
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2024-01-05 17:19:03 +0530

    Simple calculator
    Enter operator: +, -, *, /: 
    Enter two operands: 
    2 + 30 = 32

---
+++ SimpleCalculator.cpp
@@ -0,0 +1,51 @@
+
+#include<iostream>
+using namespace std;
+
+void fastio(bool read = false) {
+	if(read) {
+		#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+		freopen("error.txt","w",stderr);
+		#endif
+	}
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	return;
+}
+
+int main(){
+    fastio(true);
+  char op;
+  float num1, num2;
+  cout << "Enter operator: +, -, *, /: "<<endl;
+  cin >> op;
+  cout << "Enter two operands: "<<endl;
+  cin >> num1 >> num2;
+
+  switch(op) {
+
+    case '+':
+      cout << num1 << " + " << num2 << " = " << num1 + num2;
+      break;
+
+    case '-':
+      cout << num1 << " - " << num2 << " = " << num1 - num2;
+      break;
+
+    case '*':
+      cout << num1 << " * " << num2 << " = " << num1 * num2;
+      break;
+
+    case '/':
+      cout << num1 << " / " << num2 << " = " << num1 / num2;
+      break;
+
+    default:
+      // If the operator is other than +, -, * or /, error message is shown
+      cout << "Error! operator is not correct";
+      break;
+  }
+}
