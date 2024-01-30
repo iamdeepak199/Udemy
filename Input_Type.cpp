ed39b33a20f3edb9e8703baf849970946409f4a2
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2024-01-31 02:20:00 +0530

    Input_type
    Enter a String Which may br contains Symbol,Alphabets,Digits ect :
    Hello * Deepak2201 - <this> ? is (a) server call 
    Alphabets Are in String : 17
    Digits Are in String : 4
    Whitespace Are in String : 6
    Special Symbol Are in String : 22

---
+++ Input_Type.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<cctype>
+#include<cstring>
+using namespace std;
+
+/*bool comparePassword(char Password[],char expectedPassword[]){
+    int i=0;
+    while(Password[i]!='\0'){
+        if(Password[i]!=expectedPassword[i]){
+           return false;
+        }
+        i++;
+}
+return true;
+
+}*/
+int main() {
+     char word[100];
+     cout << "Enter a String Which may br contains Symbol,Alphabets,Digits ect :" <<endl;
+     cin.getline(word,1000,'$');
+     cout<<word<<endl;
+     int Alphabets=0,Symbol=0,Digits=0,Whitespace=0;
+     for(int i=0;i<word[i]!='\0';i++){
+         char ch=word[i];
+         if(isalpha(ch)){
+            Alphabets++;
+         }
+         else if(isdigit(ch)){
+            Digits++;
+         }
+         else if(isspace(ch)){
+            Whitespace++;
+         }
+     }
+     cout<<"Alphabets Are in String : "<<Alphabets<<endl;
+     cout<<"Digits Are in String : "<<Digits<<endl;
+     cout<<"Whitespace Are in String : "<<Whitespace<<endl;
+    Symbol= (strlen(word)-(Alphabets + Digits + Whitespace));
+     cout<<"Special Symbol Are in String : "<<Symbol<<endl;
+
+    
+      
+}
