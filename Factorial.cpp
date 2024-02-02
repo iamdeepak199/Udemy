2dd5f3791faeb7b0cf8ce62c2029f4b0bd640c70
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2024-02-02 20:39:24 +0530

    Factorial Of N
    Enter a number to find it's Factorial : 
    Factorial Of : 5 Is : 120

--- Factorial.cpp
+++ Factorial.cpp
@@ -1,29 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void fastio(bool read = false) {
-	if(read) {
-		#ifndef ONLINE_JUDGE
-		freopen("input.txt","r",stdin);
-		freopen("output.txt","w",stdout);
-		freopen("error.txt","w",stderr);
-		#endif
-	}
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	return;
+int Factorial(int n){
+    if(n==1 || n==0){
+        return 1;
+    }
+   else{
+       return (n*Factorial(n-1));
+   }
 }
-
-int main(){
-    fastio(true);
-   int N,Fact=1;
-    cout<<"Enter a number to find it's Factorial ! : ";
-    cin>>N;
-    cout<<N<<endl;
-    for(int i=1;i<=N;i++){
-        Fact*=i;
-    }
-    cout<<"Factorial of a number is :"<<Fact;
-   
-    }
+int main() {
+   int n=5;
+   cout << "Enter a number to find it's Factorial : "<<endl;
+   //cin>>n;
+   cout<<"Factorial Of : "<<n<<" Is : "<<Factorial(n);
+  
+}
